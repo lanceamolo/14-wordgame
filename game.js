bab576329dd3fbd1456a2fb5a913296f31636034
Punchcode <punchcode@Punchcodes-MacBook-Pro.local>
2020-09-22 17:02:13 -0700

    tuesday update

--- game.js
+++ game.js
@@ -14,36 +14,51 @@ for (let i = 0; i < chosenWord.length; i++) {
 }
 console.log(answerArr)
 
-// let currentWord = []
-// const hideLetters = (word) => {
-//   for (let i = 0; i < chosenWord.length; i++) {
-//     return (currentWord = currentWord + "_")
-//   }
-// }
-
-// document.querySelector("#answerDiv").innerHTML = answerArray
+let currentWord = []
+const hideLetters = (word) => {
+  for (let i = 0; i < chosenWord.length; i++) {
+    return (currentWord = currentWord + "_")
+  }
+}
 
 let userTurns = 8
 const youLose = false
 if (userTurns === 0) {
   return (youLose = true)
 }
-console.log(`You're on turn #${userTurns}`)
+// console.log(`You're on turn #${userTurns}`)
 
-let userInput = prompt("Guess a letter")
-if (userInput !== undefined) {
-  userTurns = userTurns - 1
-}
+const startGame = document.querySelector("#startGame")
+startGame.addEventListener("click", () => {
+  document.querySelector("#blankArea").innerHTML = answerArr.join("")
+})
 
-let currentArr = []
-const guess = (word, letter) => {
-  for (let j = 0; j < word.length; j++) {
-    if (word[j] === letter) {
-      currentArr = answerArr[j] = letter
+const guessButton = document.getElementById("guessButton")
+
+guessButton.addEventListener("click", function (e) {
+  e.preventDefault()
+  const letterInput = document.getElementById("letterInput")
+  const userInput = letterInput.value
+
+  if (userInput.length == 0) {
+    alert("You need to insert a letter")
+  } else {
+    let currentArr = []
+    const guess = (word, letter) => {
+      for (let j = 0; j < word.length; j++) {
+        if (word[j] === letter) {
+          currentArr = answerArr[j] = letter
+        }
+      }
     }
+    guess(chosenWord, userInput)
+    console.log(currentArr)
+    console.log(answerArr)
+    document.querySelector("#blankArea").innerHTML = answerArr.join("")
+    letterInput.value = ""
   }
-}
+})
 
-guess(chosenWord, userInput)
-console.log(answerArr)
-console.log(`You're on turn #${userTurns}`)
+// if (userInput !== undefined) {
+//   userTurns = userTurns - 1
+// }
